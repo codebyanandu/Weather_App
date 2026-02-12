1d4dafaa9c999ae73e3db6124d67138f84349ddb
Anandu <ananduajithkumar123@gmail.com>
2026-02-12 12:39:26 +0530

    Updated Splash Page

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app/app/utils/colors.dart';
+import 'package:weather_app/app/utils/constants.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(backgroundColor: AColors.primaryClr);
+    return Scaffold(
+      backgroundColor: AColors.primaryClr,
+      body: Center(
+        child: Text(
+          kAppName,
+          style: TextStyle(
+            color: AColors.white,
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+            fontFamily: 'monospace'
+          ),
+        ),
+      ),
+    );
   }
 }
