73962fff7a4b215c14a3fd79ed46e26ad9114c13
Anandu <ananduajithkumar123@gmail.com>
2026-02-15 23:39:39 +0530

    Added home Screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,73 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+   final TextEditingController _controller = TextEditingController();
+  String _city = "London";
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Weather"),
+        centerTitle: true,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            /// Search box
+            Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    controller: _controller,
+                    decoration: InputDecoration(
+                      hintText: "Enter city",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      contentPadding:
+                          const EdgeInsets.symmetric(horizontal: 12),
+                    ),
+                  ),
+                ),
+                const SizedBox(width: 8),
+                ElevatedButton(
+                  onPressed: () {
+                    final text = _controller.text.trim();
+                    if (text.isNotEmpty) {
+                      setState(() => _city = text);
+                    }
+                  },
+                  child: const Text("Search"),
+                )
+              ],
+            ),
+
+            const SizedBox(height: 24),
+
+            /// Weather state
+            // Expanded(
+            //   child: Center(
+            //     child: weatherAsync.when(
+            //       data: (weather) => WeatherCard(weather: weather),
+            //       loading: () => const CircularProgressIndicator(),
+            //       error: (e, _) => Text(
+            //         "City not found",
+            //         style: TextStyle(color: Colors.red.shade400),
+            //       ),
+            //     ),
+            //   ),
+            // ),
+          ],
+        ),
+      ),
+    );
   }
 }
